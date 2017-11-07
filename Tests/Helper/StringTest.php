b79706412443fef493eff347c560f637b9843d9f
Thomas Hanke <thomas@it-hanke.de>
2017-11-07 13:36:13 +0100

    add more tests fixing

--- Tests/Helper/StringTest.php
+++ Tests/Helper/StringTest.php
@@ -43,7 +43,7 @@ class StringTest extends TestCase
     public function testFindLast()
     {
         $string = new \TeamSpeak3_Helper_String("Hello World!");
-        $this->assertEquals(9, $string->findFirst("l"));
+        $this->assertEquals(2, $string->findFirst("l"));
     }
 
     public function testToLower()
