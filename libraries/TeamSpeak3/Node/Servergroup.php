75766001d7c1aeb608bf174d291ea23fd398aeb3
Sven Paulsen <info@sven-paulsen.de>
2018-03-24 20:13:10 +0100

    fixed invalid return type

--- libraries/TeamSpeak3/Node/Servergroup.php
+++ libraries/TeamSpeak3/Node/Servergroup.php
@@ -147,7 +147,7 @@ class TeamSpeak3_Node_Servergroup extends TeamSpeak3_Node_Abstract
   /**
    * Returns a list of clients assigned to the server group specified.
    *
-   * @return array|TeamSpeak3_Node_Client[]
+   * @return array
    */
   public function clientList()
   {
