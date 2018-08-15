6e309e651d9c11ad5ad17d2167ae8dfcdfcb2747
Yoni Lerner <yonilerner@gmail.com>
2018-08-14 18:38:50 -0700

    Alias PHPUnit isType if PHP unit version is low also

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,11 @@ if (!file_exists($file)) {
 
 $autoload = require_once $file;
 
+$parsed_phpunit_version = `./vendor/bin/phpunit --version | head -n 1 | tr ' ' '\n' | head -n 2 | tail -n 1 | head -c 1`;
+if (!is_null($parsed_phpunit_version) && $parsed_phpunit_version < 6) {
+    class_alias('PHPUnit_Framework_Constraint_IsType', 'PHPUnit\Framework\Constraint\IsType');
+}
+
 // Make PHPUnit 6 tests backward compatible for PHPUnit 5 code base
 if (PHP_VERSION_ID < 70000) {
   class_alias('PHPUnit_Framework_Constraint_IsType', 'PHPUnit\Framework\Constraint\IsType');
