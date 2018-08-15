059ff1438f3084361926b3899f91b42aead14213
Yoni Lerner <yonilerner@gmail.com>
2018-08-15 01:28:03 -0700

    Use PHPUnits own version reporting rather than parsing exec output

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,12 +10,7 @@ if (!file_exists($file)) {
 
 $autoload = require_once $file;
 
-$parsed_phpunit_version = `./vendor/bin/phpunit --version | head -n 1 | tr ' ' '\n' | head -n 2 | tail -n 1 | head -c 1`;
-if (!is_null($parsed_phpunit_version) && $parsed_phpunit_version < 6) {
-    class_alias('PHPUnit_Framework_Constraint_IsType', 'PHPUnit\Framework\Constraint\IsType');
-}
-
 // Make PHPUnit 6 tests backward compatible for PHPUnit 5 code base
-if (PHP_VERSION_ID < 70000) {
+if (PHP_VERSION_ID < 70000 || PHPUnit_Runner_Version::id() < 6) {
   class_alias('PHPUnit_Framework_Constraint_IsType', 'PHPUnit\Framework\Constraint\IsType');
 }
