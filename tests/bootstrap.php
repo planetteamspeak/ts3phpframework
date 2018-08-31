0e3171138ca4c0812f2cda519b7639de43081c08
Samuele Negrini <negrinisamuele@outlook.com>
2018-08-31 16:13:18 +0200

    Fixed PHPUnit version check for versions >= 6.0 (#100)
    * Fixed PHPUnit version check for versions >= 6.0
    
    * Added further check for Travis CI build

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,13 @@ if (!file_exists($file)) {
 
 $autoload = require_once $file;
 
+// todo: combine backward-compatibility checks by existance rather than version.
+// Creating an alias for PHPUnit version check
+if (class_exists('PHPUnit_Runner_Version')) {
+    class_alias ('PHPUnit_Runner_Version', 'PHPUnit\Runner\Version');
+}
+
 // Make PHPUnit 6 tests backward compatible for PHPUnit 5 code base
-if (PHP_VERSION_ID < 70000 || PHPUnit_Runner_Version::id() < 6) {
+if (PHP_VERSION_ID < 70000 || PHPUnit\Runner\Version::id() < 6) {
   class_alias('PHPUnit_Framework_Constraint_IsType', 'PHPUnit\Framework\Constraint\IsType');
 }
