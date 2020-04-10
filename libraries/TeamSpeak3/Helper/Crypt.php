41a40e5f79b645fa2a0fd6cdfea00537748d169c
Fjonan <fjonan@fjonan.com>
2020-04-10 14:49:58 +0200

    removed deprecated curly braces from crypt.php

--- libraries/TeamSpeak3/Helper/Crypt.php
+++ libraries/TeamSpeak3/Helper/Crypt.php
@@ -174,7 +174,7 @@ class TeamSpeak3_Helper_Crypt
       $data = 0;
       for($j = 4; $j > 0; $j--)
       {
-        $data = $data << 8 | ord($passphrase{$k});
+        $data = $data << 8 | ord($passphrase[$k]);
         $k = ($k+1) % $length;
       }
       $this->p[$i] ^= $data;
